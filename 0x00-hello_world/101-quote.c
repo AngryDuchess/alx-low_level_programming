6ceb5a4fdba8ce18e75b18b692dc7596df8276d4
Hamida Mahama <mahamahamida@gmail.com>
2023-04-09 11:21:33 +0000

    hell c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ * main - Entry point of program.
+ * Description: printing out a sentence without using 
+ * the functions listed in the NAME section of the man (3)
+ * printf or man (3) puts
+ *
+ * Return: Always 0(success)
+ */
+
+int main()
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return 1;
+}
