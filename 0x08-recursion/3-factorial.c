246cf0cb363bc546d8bb5ca117fc74ae1ed751ad
AngryDuchess <mahamahamida@gmail.com>
2023-06-09 04:14:58 -0700

    recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * factorial - calculates the factorial of a number
+ * @n: number to be checked
+ *
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+	if (n == 1)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+	
+		return (n * factorial(n - 1));
+}
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - returns value of x raised to the power of y
+ * @x: main value 
+ * @y: exponential value
+ * Return: Alwasy 0,
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	else return (x * _pow_recursion(x, y - 1));
+}
