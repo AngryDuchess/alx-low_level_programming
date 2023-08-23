0219b551bad8f990615489a00ca1a3545d6489cb
Mahama Hamida <mahamahamida@gmail.com>
2023-08-23 01:03:54 -0700

    jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wger -P /tmp https://github.c
+wget -P /tmp https://github.com/AngryDuchess/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win_preload.so
+export LD_PRELOAD=/tmp/win_preload.so
